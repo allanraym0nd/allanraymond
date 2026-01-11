259e2d367093202d32dccfceeb1355d7c8b43ae4
allan raymond <raymonda916@gmail.com>
2026-01-11 18:12:16 +0300

    added Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,18 @@
-const Home = () => { }
+import { motion } from 'framer-motion';
+import { TypeAnimation } from 'react-type-animation';
+// import SpotifyPlaying from '../components/SpotifyPlaying';
+// import YouTubeLatest from '../components/YouTubeLatest';
+// import { usePageTitle } from '../hooks/usePageTitle';
+// import { FadeInSection } from '../utils/FadeInSection';
+
+const Home = () => {
+    // usePageTitle(' ')
+
+    return (
+        <></>
+    )
+}
+
+
 
 export default Home;
