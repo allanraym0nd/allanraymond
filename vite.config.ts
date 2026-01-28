67c50c92b8772fe474239be3c782e585af1784ee
allan raymond <raymonda916@gmail.com>
2026-01-28 11:20:13 +0300

    prod is sth alright

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    build: {
-        // This tells Vite to continue even with TS errors
-        rollupOptions: {
-            onwarn(warning, warn) {
-                // Suppress all warnings
-                return;
-            }
-        }
-    }
+    // build: {
+    //     // This tells Vite to continue even with TS errors
+    //     rollupOptions: {
+    //         onwarn(warning, warn) {
+    //             // Suppress all warnings
+    //             return;
+    //         }
+    //     }
+    // }
 })
