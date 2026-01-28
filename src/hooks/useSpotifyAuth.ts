a715c3be716b58ebfbfff37158369191c66ba0c5
allan raymond <raymonda916@gmail.com>
2026-01-28 10:38:13 +0300

    fix:comment out spotify auth for deployment

--- src/hooks/useSpotifyAuth.ts
+++ src/hooks/useSpotifyAuth.ts
@@ -1,45 +1,45 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID
-const CLIENT_SECRET = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
-const REFRESH_TOKEN = import.meta.env.VITE_SPOTIFY_REFRESH_TOKEN;
-const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+// // const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID
+// // const CLIENT_SECRET = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
+// // const REFRESH_TOKEN = import.meta.env.VITE_SPOTIFY_REFRESH_TOKEN;
+// // const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 
-export const useSpotifyAuth = () => {
-    const [token, setToken] = useState<string | null>(null)
+// export const useSpotifyAuth = () => {
+//     const [token, setToken] = useState<string | null>(null)
 
-    useEffect(() => {
-        if (!token) {
-            getAccessToken().then((data) => {
-                setToken(data.access_token)
-            }).catch((error) => {
-                console.error('Error fetching Spotify access token:', error)
-            })
+//     useEffect(() => {
+//         if (!token) {
+//             getAccessToken().then((data) => {
+//                 setToken(data.access_token)
+//             }).catch((error) => {
+//                 console.error('Error fetching Spotify access token:', error)
+//             })
 
-        }
+//         }
 
-    }, [])
+//     }, [])
 
-    const getAccessToken = async () => {
-        const basic = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)
-        const params = new URLSearchParams
-        params.append("grant_type", "refresh_token")
-        params.append("refresh_token", REFRESH_TOKEN)
+//     const getAccessToken = async () => {
+//         const basic = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)
+//         const params = new URLSearchParams
+//         params.append("grant_type", "refresh_token")
+//         params.append("refresh_token", REFRESH_TOKEN)
 
-        const response = await fetch('TOKEN_ENDPOINT', {
-            method: 'POST',
-            headers: {
-                Authorization: `Basic ${basic}`,
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: params.toString()
-        })
+//         const response = await fetch('TOKEN_ENDPOINT', {
+//             method: 'POST',
+//             headers: {
+//                 Authorization: `Basic ${basic}`,
+//                 'Content-Type': 'application/x-www-form-urlencoded',
+//             },
+//             body: params.toString()
+//         })
 
-        return response.json()
+//         return response.json()
 
-    }
+//     }
 
-    return { token }
+//     return { token }
 
-}
+// }
